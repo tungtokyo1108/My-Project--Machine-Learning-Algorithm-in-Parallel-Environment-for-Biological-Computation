6c6c306ca57ad0b24866826b97fcae298741227f
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-03-26 09:48:54 +0900

    Update BipartitionList.h

--- BipartitionList.h
+++ BipartitionList.h
@@ -1,15 +1,9 @@
 
 /********************
 
-PhyloBayes MPI. Copyright 2010-2013 Nicolas Lartillot, Nicolas Rodrigue, Daniel Stubbs, Jacques Richer.
-
-PhyloBayes is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-PhyloBayes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-See the GNU General Public License for more details. You should have received a copy of the GNU General Public License
-along with PhyloBayes. If not, see <http://www.gnu.org/licenses/>.
+Adopted PhyloBayes MPI. https://github.com/bayesiancook/pbmpi
+Lartillot, N., Rodrigue, N., Stubbs, D. & Richer, J. 
+PhyloBayes MPI: Phylogenetic reconstruction with infinite mixtures of profiles in a parallel environment. Syst. Biol. 62, 611–615 (2013).
 
 **********************/
 
